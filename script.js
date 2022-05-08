829447b4b7ab15471f2dd439e637d16b68e8574f
FUMIAKI-ICHIKI <53174787+FUMIAKI-ICHIKI@users.noreply.github.com>
2022-05-09 08:00:48 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,11 @@
 let elements = document.getElementsByName("flexRadioDefault");
 let resultImg = document.getElementById("resultImg");
 let resultText = document.getElementById("resultText");
+let nowplaying = document.getElementById("nowPlaying")
 
 document.getElementById("decideBtn").onclick = function() {
+    resultImg.src = "./images/readygo.png";
+    resultText.innerHTML = "スタート";
     //入力(ラジオボタン)をチェック
     let checkValue = ""
     for (let i = 0; i < elements.length; i++) {
@@ -20,35 +23,52 @@ document.getElementById("decideBtn").onclick = function() {
         1: "scissors",
         2: "paper"
     };
-    //勝ち負け・あいこ　を確認
-    if (checkValue === objectOfsign[mySignkey]) {
-        resultImg.src = "./images/draw.png";
-        resultText.innerHTML = "あいこです";
-    } else if (checkValue === "rock") {
-        if (objectOfsign[mySignkey] === "scissors") {
-            resultImg.src = "./images/win.png";
-            resultText.innerHTML = "あなたの勝ちです";
-        } else {
-            resultImg.src = "./images/lose.png";
-            resultText.innerHTML = "あなたの負けです";
-        }
-    } else if (checkValue === "scissors") {
-        if (objectOfsign[mySignkey] === "paper") {
-            resultImg.src = "./images/win.png";
-            resultText.innerHTML = "あなたの勝ちです";
-        } else {
-            resultImg.src = "./images/lose.png";
-            resultText.innerHTML = "あなたの負けです";
-        }
-    } else if (checkValue === "paper") {
-        if (objectOfsign[mySignkey] === "rock") {
-            resultImg.src = "./images/win.png";
-            resultText.innerHTML = "あなたの勝ちです";
-        } else {
-            resultImg.src = "./images/lose.png";
-            resultImg.innerHTML = "あなたの負けです";
 
+    nowplaying.innerHTML = "じゃん"
 
-        }
+    function callKen() {
+        nowplaying.innerHTML = "けん";
     }
+
+    function callPon() {
+        nowplaying.innerHTML = "ぽん";
+    }
+
+    setTimeout(callKen, 500);
+    setTimeout(callPon, 1000);
+
+    setTimeout(function() {
+
+        //勝ち負け・あいこ　を確認
+        if (checkValue === objectOfsign[mySignkey]) {
+            resultImg.src = "./images/draw.png";
+            resultText.innerHTML = "あいこです";
+        } else if (checkValue === "rock") {
+            if (objectOfsign[mySignkey] === "scissors") {
+                resultImg.src = "./images/win.png";
+                resultText.innerHTML = "あなたの勝ちです";
+            } else {
+                resultImg.src = "./images/lose.png";
+                resultText.innerHTML = "あなたの負けです";
+            }
+        } else if (checkValue === "scissors") {
+            if (objectOfsign[mySignkey] === "paper") {
+                resultImg.src = "./images/win.png";
+                resultText.innerHTML = "あなたの勝ちです";
+            } else {
+                resultImg.src = "./images/lose.png";
+                resultText.innerHTML = "あなたの負けです";
+            }
+        } else if (checkValue === "paper") {
+            if (objectOfsign[mySignkey] === "rock") {
+                resultImg.src = "./images/win.png";
+                resultText.innerHTML = "あなたの勝ちです";
+            } else {
+                resultImg.src = "./images/lose.png";
+                resultImg.innerHTML = "あなたの負けです";
+
+
+            }
+        }
+    }, 1500)
 }
